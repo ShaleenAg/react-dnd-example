f990c220b1cb917df0a9300473ae26cb13e16f54
ShaleenAg <44058516+ShaleenAg@users.noreply.github.com>
2022-04-26 11:44:19 +0530

    Update Board.tsx

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,13 +1,13 @@
-import type { CSSProperties, FC } from 'react'
-import { useEffect, useState } from 'react'
+import type { CSSProperties, FC } from 'react';
+import { useEffect, useState } from 'react';
 
-import { BoardSquare } from './BoardSquare'
-import type { Game, Position } from './Game'
-import { Piece } from './Piece'
-import Dummy from './Dummy'
-import {Menu, MenuItem, Button} from "@mui/material"
+import { BoardSquare } from './BoardSquare';
+import type { Game, Position } from './Game';
+import { Piece } from './Piece';
+import Dummy from './Dummy';
+import { Menu, MenuItem, Button } from '@mui/material';
 export interface BoardProps {
-  game: Game
+  game: Game;
 }
 
 /** Styling properties applied to the board element */
@@ -16,24 +16,22 @@ const boardStyle: CSSProperties = {
   height: '100%',
   display: 'flex',
   flexWrap: 'wrap',
-}
+};
 /** Styling properties applied to each square element */
-const squareStyle: CSSProperties = { width: '12.5%', height: '12.5%' }
+const squareStyle: CSSProperties = { width: '12.5%', height: '12.5%' };
 
 /**
  * The chessboard component
  * @param props The react props
  */
 export const Board: FC<BoardProps> = ({ game }) => {
-  const [[knightX, knightY], setKnightPos] = useState<Position>(
-    game.knightPosition,
-  )
+  const [[knightX, knightY], setKnightPos] = useState<Position>(game.knightPosition);
   const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
-  useEffect(() => game.observe(setKnightPos))
+  useEffect(() => game.observe(setKnightPos));
 
   function renderSquare(i: number) {
-    const x = i % 8
-    const y = Math.floor(i / 8)
+    const x = i % 8;
+    const y = Math.floor(i / 8);
 
     return (
       <div key={i} style={squareStyle}>
@@ -41,28 +39,15 @@ export const Board: FC<BoardProps> = ({ game }) => {
           <Piece isKnight={x === knightX && y === knightY} />
         </BoardSquare>
         <div className="menu">
-          <Button onClick={(event) => setMenuAnchor(event.currentTarget)}>Open Menu</Button>
-          <Menu
-          open={Boolean(menuAnchor)}
-          anchorEl={menuAnchor}
-          onClose={()=>setMenuAnchor(null)}
-          >
-            {Array(9)
-              .fill(0)
-              .map(() => (
-                <MenuItem>
-                  <Dummy />
-                </MenuItem>
-              ))}
-          </Menu>
+          <Dummy />
         </div>
       </div>
     );
   }
 
-  const squares = []
+  const squares = [];
   for (let i = 0; i < 64; i += 1) {
-    squares.push(renderSquare(i))
+    squares.push(renderSquare(i));
   }
-  return <div style={boardStyle}>{squares}</div>
-}
+  return <div style={boardStyle}>{squares}</div>;
+};
